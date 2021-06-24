98ff4fa2ff15572ed1709c989c71ebf21f0cee53
urmiladhanala <urmila.dhanala@gmail.com>
2021-06-24 18:32:09 +0530

    second file

---
+++ exer2.java
@@ -0,0 +1,10 @@
+public class exer2 {//compute quotient and remainder 
+    public static void main(String[] args){
+    int a = 20;
+    int b = 5;
+    int quotient = a / b;
+    int remainder= a % b;
+    System.out.println("Quotient is "+ quotient );
+    System.out.println("Remainder is " + remainder);
+    }
+}
